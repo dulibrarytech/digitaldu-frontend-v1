e33e99ee4a2460bdf0c592375ab06f6031bf95ac
Jeff Rynhart <jeff.rynhart@du.edu>
2020-02-13 11:21:32 -0700

    Apply client search form sanitization update

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -14,6 +14,23 @@ $( document ).ready(function() {
 		window.location.replace(encodeURI(searchUrl));
 	});
 
+	$("#home-search button").click(function(event) {
+		event.preventDefault();
+		$("#search-box input").val(DOMPurify.sanitize($("#search-box input").val()));
+		$("#home-search").submit();
+	});
+
+	$(".banner-search button").click(function(event) {
+		event.preventDefault()
+		$(".banner-search form input[type='text']").val(DOMPurify.sanitize($(".banner-search form input[type='text']").val()));
+		$(".banner-search form").submit();
+	});
+
+	$(".sidebar-search button").click(function(event) {
+		event.preventDefault();
+		$(".sidebar-search input[type='text']").val(DOMPurify.sanitize($(".sidebar-search input[type='text']").val()));
+		$(".sidebar-search form").submit();
+	});
 });
 
 
